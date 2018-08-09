35bc6f8d7e5e90b61b0e913f120c99fc4516675b
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-08-09 15:21:55 -0400

    styling
    temp style

---
+++ js/tablero.js
@@ -0,0 +1,29 @@
+let app={
+    init: function(){
+        
+    let btns=document.querySelectorAll(".switch");
+    btns.forEach(swt=>{
+        swt.addEventListener("click", app.getId);
+    })
+    },
+    getId: function(ev){
+    let ids=ev.currentTarget.getAttribute("id");
+        switch(ids){
+            case "op1":
+            (document.getElementById("op1").style.backgroundColor="white")&&(document.getElementById("op2").style.backgroundColor="transparent");
+                app.showText1()
+                break;
+            case "op2":
+        (document.getElementById("op2").style.backgroundColor="white")&&(document.getElementById("op1").style.backgroundColor="transparent")
+                app.showText2();
+                break;
+        }
+},
+    showText1: function(){
+        console.log("This is text 1")
+    },
+    showText2: function(){
+        console.log("this is text 2")
+    }
+}
+document.addEventListener("DOMContentLoaded", app.init);
